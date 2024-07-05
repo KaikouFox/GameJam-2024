5e36aae035ca3ed600b0a5b2dbdf488cff3833ce
KaikouFox <cacauboon.vangember@gmail.com>
2024-07-05 10:08:08 +0200

    added action points

--- GameJam 2024/Assets/Scripts/PlayerController.cs
+++ GameJam 2024/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float baseSpeed = 5f;
+    [SerializeField] private int actionPoints = 10;
 
     private float movementX;
     private float movementY;
+    private CanvasManager canvasManager;
+
+    private void Start()
+    {
+        canvasManager = FindObjectOfType<CanvasManager>();
+        canvasManager.SetActionPoints(actionPoints);
+    }
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ChangePointAmount(-1);
+        }
     }
 
     private void FixedUpdate()
@@ -20,5 +31,11 @@ public class PlayerController : MonoBehaviour
         movementY = Input.GetAxisRaw("Vertical");
 
         transform.position = transform.position + new Vector3(movementX * baseSpeed * Time.deltaTime, movementY * baseSpeed * Time.deltaTime, 0);
+    }
+
+    public void ChangePointAmount(int newValue)
+    {
+        actionPoints += newValue;
+        canvasManager.SetActionPoints(actionPoints);
     }
 }
